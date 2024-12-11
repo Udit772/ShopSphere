307584cf2fe378c354593d880f82f3bccc99b735
Udit Singh <uditsinghsfc@gmail.com>
2024-12-11 16:41:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //configure env
 dotenv.config();
